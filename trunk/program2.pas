c8245a180a3008bc1d9e16c1001d649036a5d391
AMaczynski <artur.m1717@gmail.com>
2016-11-17 13:23:48 +0100

    Drugi program

---
+++ trunk/program2.pas
@@ -0,0 +1,33 @@
+PROGRAM GAME;
+USES CRT;
+VAR X,Y,C,ILOSC:INTEGER;
+ODP:CHAR;
+KONIEC:BOOLEAN;
+BEGIN
+REPEAT;
+    CLRSCR;
+        RANDOMIZE;
+    X:=(RANDOM(C));
+        writeln('Wpisz z ilu liczb ba losowac?');
+        readln(C);
+    TEXTCOLOR(2);
+    GOTOXY(30,5);
+    WRITELN(' TO JEST GRA ');
+    REPEAT
+    TEXTCOLOR(8);
+    WRITE(' PODAJ WARTOÆ LICZBY');
+    READLN(Y);
+    ILOSC:=ILOSC+1;
+    TEXTCOLOR(3);
+    IF (X<Y) THEN WRITELN('TA GRA CI PRZERASTA-liczba za duza ');
+    TEXTCOLOR(5);
+    IF (X>Y) THEN WRITELN('NIGDY NIE ZGADNIESZ-LICZBA za mala');
+    TEXTCOLOR(6);
+    IF (X=Y) THEN WRITELN('CHYBA KTOs CI POMAGA ?');
+    UNTIL X=Y;
+    WRITELN('ZGADYWA£E ',ILOSC,' RAZY');
+    WRITE('Czy zagrasz jeszcze raz? (Y/N)');
+    READLN(ODP);
+    KONIEC:=NOT ((ODP='y') OR (ODP='Y'));
+    UNTIL KONIEC;
+END.
